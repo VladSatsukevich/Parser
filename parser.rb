488ff4a071e4b1fd3fb3aa238d5755dd074faa7e
Vlad <84670334+VladSatsukevich@users.noreply.github.com>
2022-02-19 14:18:46 +0300

    Update parser.rb

--- parser.rb
+++ parser.rb
@@ -9,10 +9,10 @@ def initialization
   main_page = gets.chomp
   puts "Preparing to scrapping!!!"
   csv_create(file_name)
-  main_parse(file_name, main_page)
+  parse_category(file_name, main_page)
 end
 
-def download_method(main_page)
+def parse_product(main_page)
   page = Curl.get(main_page)
   download_page = Nokogiri::HTML(page.body_str)
   puts "Scrapping #{main_page}..."
@@ -20,7 +20,7 @@ def download_method(main_page)
 end
 
 def product_parsing(file_name, url)
-  download_page = download_method(url + '.html')
+  download_page = parse_product(url + '.html')
   name  = download_page.xpath('//h1[@class = "product_main_name"]/text()')
   image = download_page.xpath('//img[@class = "replace-2x img-responsive"]/@src')
   variations = download_page.xpath('//div[contains(@class, "attribute_list")]/ul/li')
@@ -44,8 +44,8 @@ def csv_create (file_name)
   end
 end
 
-def main_parse(file_name, category)
-  number_of_products = download_method(category).xpath('//input[@id="nb_item_bottom"]/@value').text.to_i
+def parse_category(file_name, category)
+  number_of_products = parse_product(category).xpath('//input[@id="nb_item_bottom"]/@value').text.to_i
   number_of_pages = (number_of_products / 25.0).ceil
   page_parsing(file_name, category)
   (2..number_of_pages).each do |page_number|
@@ -55,11 +55,11 @@ def main_parse(file_name, category)
 end
 
 def page_parsing(file_name, main_page)
-  download_page = download_method(main_page)
-  link = download_page.xpath("//*[@class = 'product_img_link pro_img_hover_scale product-list-category-img']/@href")
-  link_list = link.to_s.split(/.html/)
+  download_page = parse_product(main_page)
+  parse_product_url = download_page.xpath("//*[@class = 'product_img_link pro_img_hover_scale product-list-category-img']/@href")
+  parse_url_list = parse_product_url.to_s.split(/.html/)
   puts "Start parsing products page:"
-  link_list.each { |url_link| product_parsing(file_name, url_link)}
+  parse_url_list.each { |url_link| product_parsing(file_name, url_link)}
 end
 
 initialization
